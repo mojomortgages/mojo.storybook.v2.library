3f21540d406d8ca73690229eda84ca7f043a07a9
Bitbucket Pipelines <bitbucket@mojomortgages.com>
2020-05-27 12:08:59 +0000

    Build 1.0.26

--- src/SuffixInput.js
+++ src/SuffixInput.js
@@ -10,6 +10,7 @@ var _rollupPluginBabelHelpers = require('./_rollupPluginBabelHelpers-4edf0050.js
 var React = require('react');
 var React__default = _interopDefault(React);
 var index = require('./index-58a12fd4.js');
+var reactNumberFormat_es = require('./react-number-format.es-086060ff.js');
 
 function _templateObject3() {
   var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  display: block;\n  border: none;\n  background: transparent;\n  width: 100%;\n  height: 100%;\n  padding: 12px 16px;\n  font-size: 16px;\n  color: ", ";\n  box-sizing: border-box;\n  outline: none;\n"]);
@@ -54,7 +55,7 @@ var Suffix = styled__default.label(_templateObject2(), function (props) {
 }, function (props) {
   return props.theme.shade[20];
 });
-var InnerInput = styled__default.input(_templateObject3(), function (props) {
+var InnerInput = styled__default(reactNumberFormat_es.NumberFormat)(_templateObject3(), function (props) {
   return props.theme.shade[0];
 });
 function SuffixInput(props) {
