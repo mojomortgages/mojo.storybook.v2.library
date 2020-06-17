0151cc705a2bc3e895e72e2d425ecf41f9f06079
Bitbucket Pipelines <bitbucket@mojomortgages.com>
2020-06-17 12:58:22 +0000

    Build 1.0.49

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -10,8 +10,28 @@ var _rollupPluginBabelHelpers = require('./_rollupPluginBabelHelpers-4edf0050.js
 var React = require('react');
 var React__default = _interopDefault(React);
 
+function _templateObject3() {
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral([""]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral([""]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
 function _templateObject() {
-  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  padding: 24px;\n  color: ", ";\n  background: ", ";\n  border-radius: ", ";\n  color: ", ";\n"]);
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  padding: 24px;\n  color: ", ";\n  background: ", ";\n  border-radius: ", ";\n  color: ", ";\n  @media (max-width: ", ") {\n  }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -27,9 +47,13 @@ var Container = styled__default.div(_templateObject(), function (props) {
   return props.theme.radius.large;
 }, function (props) {
   return props.theme.shade[0];
+}, function (props) {
+  return props.theme.maxWidth;
 });
+var Title = styled__default.div(_templateObject2());
+var Hidden = styled__default.div(_templateObject3());
 function Tooltip(props) {
-  return /*#__PURE__*/React__default.createElement(Container, null, props.children);
+  return /*#__PURE__*/React__default.createElement(Container, null, /*#__PURE__*/React__default.createElement(Title, null), /*#__PURE__*/React__default.createElement(Hidden, null, props.children));
 }
 
 exports.default = Tooltip;
