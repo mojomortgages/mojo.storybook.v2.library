f2a9b840f125d0f29a0100fa84f9c4ea6efacff8
Bitbucket Pipelines <bitbucket@mojomortgages.com>
2020-06-19 09:28:37 +0000

    Build 1.0.54

--- src/ComboBox.js
+++ src/ComboBox.js
@@ -4,13 +4,41 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-require('styled-components');
-require('./_rollupPluginBabelHelpers-7e6b80a7.js');
+var styled = require('styled-components');
+var styled__default = _interopDefault(styled);
+var _rollupPluginBabelHelpers = require('./_rollupPluginBabelHelpers-7e6b80a7.js');
 var React = require('react');
 var React__default = _interopDefault(React);
 require('./index-58a12fd4.js');
 var Input = require('./Input.js');
 
+function _templateObject2() {
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  padding: 16px;\n  cursor: pointer;\n  border-top: 1px solid ", ";\n  :hover {\n    background: ", ";\n  }\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  background: ", ";\n  border-radius: 4px;\n  overflow: auto;\n  max-height: 256px;\n  margin-top: 8px;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+var List = styled__default.div(_templateObject(), function (props) {
+  return props.theme.shade[100];
+});
+var ListItem = styled__default.div(_templateObject2(), function (props) {
+  return props.theme.shade[60];
+}, function (props) {
+  return props.theme.shade[80];
+});
 function ComboBox(_ref) {
   var placeholder = _ref.placeholder,
       value = _ref.value,
@@ -21,8 +49,8 @@ function ComboBox(_ref) {
     value: value,
     onChange: onInputChanged,
     placeholder: placeholder
-  }), /*#__PURE__*/React__default.createElement("ul", null, comboSelections && comboSelections.map(function (selection) {
-    return /*#__PURE__*/React__default.createElement("li", {
+  }), /*#__PURE__*/React__default.createElement(List, null, comboSelections && comboSelections.map(function (selection) {
+    return /*#__PURE__*/React__default.createElement(ListItem, {
       key: selection.text,
       onClick: function onClick() {
         return onSelectionMade(selection);
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -11,7 +11,7 @@ var React = require('react');
 var React__default = _interopDefault(React);
 
 function _templateObject7() {
-  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  grid-area: icon;\n  width: 24px;\n  height: 24px;\n  background: red;\n  @media (min-width: ", ") {\n    display: none;\n  }\n"]);
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  grid-area: icon;\n  width: 18px;\n  height: 18px;\n  background: ", ";\n  border-radius: 50%;\n  @media (min-width: ", ") {\n    display: none;\n  }\n"]);
 
   _templateObject7 = function _templateObject7() {
     return data;
@@ -71,7 +71,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  padding: 24px;\n  color: ", ";\n  background: ", ";\n  border-radius: ", ";\n  color: ", ";\n  display: grid;\n  grid-auto-flow: dense;\n  grid-template-columns: auto 24px;\n  grid-gap: 8px;\n  @media (min-width: ", ") {\n    grid-template-areas: \"hidden hidden\";\n  }\n  @media (max-width: ", ") {\n    grid-template-areas: \"title icon\";\n  }\n  ", "\n"]);
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  padding: 24px;\n  color: ", ";\n  background: ", ";\n  border-radius: ", ";\n  color: ", ";\n  display: grid;\n  grid-auto-flow: dense;\n  grid-template-columns: auto 18px;\n  grid-gap: 8px;\n  @media (min-width: ", ") {\n    grid-template-areas: \"hidden hidden\";\n  }\n  @media (max-width: ", ") {\n    grid-template-areas: \"title icon\";\n  }\n  ", "\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -109,6 +109,8 @@ var Hidden = styled__default.div(_templateObject5(), function (props) {
   });
 });
 var Icon = styled__default.div(_templateObject7(), function (props) {
+  return props.theme.shade[0];
+}, function (props) {
   return props.theme.breakpoints.md;
 });
 function Tooltip(props) {
