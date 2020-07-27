c11a0fc2c35ff5693bf5b6fd74feac6f63f4720b
Bitbucket Pipelines <bitbucket@mojomortgages.com>
2020-07-27 13:03:09 +0000

    Build 1.0.102

--- src/CheckButton.js
+++ src/CheckButton.js
@@ -90,7 +90,8 @@ function CheckButton(props) {
     name: props.name,
     value: props.value,
     onChange: props.onChange,
-    onClick: props.onClick
+    onClick: props.onClick,
+    checked: props.checked
   }), /*#__PURE__*/React__default.createElement(Container, {
     htmlFor: props.id
   }, /*#__PURE__*/React__default.createElement(Checkmark, null), /*#__PURE__*/React__default.createElement(Text, null, props.children)));
