c98b5f35bc9f269e43d2d5e7e18725e09268fa1d
Bitbucket Pipelines <bitbucket@mojomortgages.com>
2020-10-26 12:42:44 +0000

    Build 1.0.149

--- src/AutoSuggest.js
+++ src/AutoSuggest.js
@@ -2116,7 +2116,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  border: 1px solid #cad1dd;\n  background: ", ";\n  position: relative;\n  border-radius: ", ";\n  &:focus-within {\n    border: 1px solid ", ";\n    box-shadow: 0 0 4px ", ";\n  }\n"]);
+  var data = _rollupPluginBabelHelpers._taggedTemplateLiteral(["\n  border: 1px solid #cad1dd;\n  background: ", ";\n  position: relative;\n  border-radius: ", ";\n  &:focus-within {\n    border: 2px solid ", ";\n    box-shadow: 0 0 4px ", ";\n  }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
